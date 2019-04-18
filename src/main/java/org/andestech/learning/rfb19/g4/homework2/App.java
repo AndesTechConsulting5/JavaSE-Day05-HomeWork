d50082d8b282535ea8e54571e7fd85efdceb6b0d
Aleksandr <akrasnyy@list.ru>
2019-04-18 23:36:26 +0300

    KrasnyyAleksandr-rfb-g4-home2

--- src/main/java/org/andestech/learning/rfb19/g4/homework2/App.java
+++ src/main/java/org/andestech/learning/rfb19/g4/homework2/App.java
@@ -8,6 +8,24 @@ public class App
 {
     public static void main( String[] args )
     {
+        Customer Ivan = new Customer("Иван","Иванов","Салтыково-Щедрина д 21 кв 86",25);
+        System.out.println(Ivan);
 
+        Customer Petr = new Customer("Петр","Петров","Умпа-лумпы д 33 кв 56",85);
+
+        DebitAccount De_Ivan = new DebitAccount(Ivan,50_000);
+        System.out.println(De_Ivan);
+
+        DebitAccount DePetr = new DebitAccount(Petr,90_000);
+        System.out.println(DePetr);
+
+
+        DePetr.putMoney(1000);
+        System.out.println(DePetr);
+
+        DePetr.withdrawal(1000);
+
+        CreditAccount  Cre_Petr = CreditAccount.CreateCredit(Petr,120_000);
+        System.out.println(Cre_Petr );
     }
 }
