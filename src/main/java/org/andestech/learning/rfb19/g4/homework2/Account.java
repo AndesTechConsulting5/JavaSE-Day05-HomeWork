f3742ace8288b2fc6fd050f260f1c0b283ea5e0b
ivan-rover <ivan-rover@mail.ru>
2019-04-18 23:21:44 +0300

    реализовано создание счетов

--- src/main/java/org/andestech/learning/rfb19/g4/homework2/Account.java
+++ src/main/java/org/andestech/learning/rfb19/g4/homework2/Account.java
@@ -2,10 +2,76 @@ package org.andestech.learning.rfb19.g4.homework2;
 
 public abstract class Account {
 
-	String customer;
-	double balance;
-	int accountId;
+	private int accountId;
+	private static double balance;
+	private Customer customer = new Customer("Иван", "Петров", "ул.Ленина, д.1", 20);
 
-	public abstract void withdrawal();
-	public abstract void putMoney();
+	private static final double MAX_BALANCE = 1_500_000, MIN_BALANCE = -200_000,
+			DELTA_MIN = 1, DELTA_MAX = 100_000;
+
+
+	public Account() {
+	}
+
+	public Account(int accountId, double balance, Customer customer) {
+		this.accountId = accountId;
+		this.balance = balance;
+		this.customer = customer;
+	}
+
+
+
+	public int getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		if(!isNewBalanceValid(balance)) {
+			return;
+		}
+		this.balance = balance;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+
+
+	public boolean isNewBalanceValid(double balance) {
+		if(balance <= MAX_BALANCE && balance >= MIN_BALANCE) {
+			return true;
+		} else {
+			System.out.println("Неверный баланс! " + balance);
+			return false;
+		}
+	}
+
+	public boolean isDeltaValid(double delta) {
+		if(delta>=0 && delta <= DELTA_MAX && delta >= DELTA_MIN) {
+			return true;
+		} else {
+			System.out.println("Неправильная сумма перевода =" + delta);
+			return false;
+		}
+
+	}
+
+	public abstract void withdrawal(double money);
+	public abstract void putMoney(double money);
+
+
+
 }
--- src/main/java/org/andestech/learning/rfb19/g4/homework2/Customer.java
+++ src/main/java/org/andestech/learning/rfb19/g4/homework2/Customer.java
@@ -2,8 +2,49 @@ package org.andestech.learning.rfb19.g4.homework2;
 
 public class Customer {
 
-	String firstName;
-	String secondName;
-	int age;
-	String address;
+	private String firstName;
+	private String secondName;
+	private int age;
+	private String address;
+
+	public Customer(String firstName, String secondName, String address, int age) {
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.age = age;
+		this.address = address;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 }
+
+
